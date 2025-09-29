7b43ea13c017b4d4fbbd26a76a6d4ee4f6da3bff
JIPING <choicanlight@gmail.com>
2025-09-30 11:58:57 +1300

    Full stack/section14/java script es6 1 (#28)
    * developer test for JS
    
    * js progress
    
    * update

--- Full_Stack/14.1 Javascript ES6/test.js
+++ Full_Stack/14.1 Javascript ES6/test.js
@@ -1,4 +1,3 @@
-
 // Go to web developer console
 // Sources - on the left (Page + Snippets + Workspace + Overrides + Content scripts)
 // Create index.js
@@ -6,7 +5,6 @@
 
 var tweet = prompt("Compose your tweet:");
 var tweetcount = tweet.length;
-
 alert("You have entered " + tweetcount + " characters, you have " + (140-tweetcount) + " characters remaining.");
 
 var tweetUnder14 = tweet.slice(0,14);
@@ -18,4 +16,3 @@ var name = prompt("what is your name: ")
 firstLetter = name.slice(0,1).toUpperCase();
 RemainingLetter = name. slice(1,);
 alert("Hello " + firstLetter+RemainingLetter + ".")
-

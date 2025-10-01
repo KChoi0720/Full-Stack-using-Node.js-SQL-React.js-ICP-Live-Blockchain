51439b339f078dbf7a2c8c1e809c4b3df6f6f57f
JIPING <choicanlight@gmail.com>
2025-10-01 15:03:08 +1300

    Full stack/secion14/java script play audio (#31)
    * play audio in website
    
    * switch statement of audio when click different button

--- Full_Stack/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Full_Stack/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,12 +1,50 @@
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i<numberOfDrumButtons; i++) {
-    
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){alert("I got clicked!");});
 
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () { alert("I got clicked!"); });
+
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+
+    var buttonInnerHTML = this.InnerHTML;
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio("sound/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom1 = new Audio("sound/tom-2.mp3");
+            tom1.play();
+            break;
+        case "s":
+            var tom1 = new Audio("sound/tom-3.mp3");
+            tom1.play();
+            break;
+        case "d":
+            var tom1 = new Audio("sound/tom-4.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom1 = new Audio("sound/snare.mp3");
+            tom1.play();
+            break;
+        case "k":
+            var tom1 = new Audio("sound/crash.mp3");
+            tom1.play();
+            break;
+        case "kl":
+            var tom1 = new Audio("sound/kick.mp3");
+            tom1.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+
+    }
+
 
 }
 
